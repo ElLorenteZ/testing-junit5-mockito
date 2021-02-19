bf9b74ebc056ed06ea35a408b3222a8703d09a8f
Robert Radzik <robertradzik@gmail.com>
2021-02-19 12:19:28 +0100

    feature: some advanced mockito

--- src/test/java/guru/springframework/sfgpetclinic/services/springdatajpa/SpecialitySDJpaServiceTest.java
+++ src/test/java/guru/springframework/sfgpetclinic/services/springdatajpa/SpecialitySDJpaServiceTest.java
@@ -2,6 +2,7 @@ package guru.springframework.sfgpetclinic.services.springdatajpa;
 
 import guru.springframework.sfgpetclinic.model.Speciality;
 import guru.springframework.sfgpetclinic.repositories.SpecialtyRepository;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -12,14 +13,13 @@ import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.BDDMockito.given;
-import static org.mockito.BDDMockito.then;
+import static org.mockito.BDDMockito.*;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class SpecialitySDJpaServiceTest {
 
-    @Mock
+    @Mock(lenient = true)
     SpecialtyRepository specialityRepository;
 
     @InjectMocks
@@ -119,4 +119,75 @@ class SpecialitySDJpaServiceTest {
         then(specialityRepository).shouldHaveNoMoreInteractions();
     }
 
+    @Test
+    void testDoThrow() {
+        doThrow(new RuntimeException("Boom")).when(specialityRepository).delete(any());
+        assertThrows(RuntimeException.class, () -> {
+            specialityRepository.delete(new Speciality());
+        });
+        verify(specialityRepository).delete(any());
+    }
+
+    @Test
+    void testFindByIdThrows() {
+        given(specialityRepository.findById(1L)).willThrow(new RuntimeException("boom"));
+
+        assertThrows(RuntimeException.class, () -> {
+            specialityRepository.findById(1L);
+        });
+
+        then(specialityRepository).should(times(1)).findById(1L);
+        then(specialityRepository).shouldHaveNoMoreInteractions();
+    }
+
+    @Test
+    void testDeleteBdd() {
+        willThrow(new RuntimeException("boom")).given(specialityRepository).delete(any());
+
+        assertThrows(RuntimeException.class, () -> {
+            specialityRepository.delete(new Speciality());
+        });
+
+        then(specialityRepository).should().delete(any());
+    }
+
+    @Test
+    void testSaveLambda() {
+        //given
+        final String MATCH_ME = "MATCH_ME";
+        Speciality speciality = new Speciality();
+        speciality.setDescription(MATCH_ME);
+
+        Speciality savedSpecialty = new Speciality();
+        savedSpecialty.setId(1L);
+
+        given(specialityRepository.save(argThat(argument -> argument.getDescription().equals(MATCH_ME)))).willReturn(savedSpecialty);
+
+        //when
+        Speciality returnedSpecialty = service.save(speciality);
+
+        //then
+        assertThat(returnedSpecialty.getId()).isEqualTo(1L);
+
+    }
+
+    @Disabled
+    @Test
+    void testSaveLambdaNoMatch(){
+        //given
+        final String MATCH_ME = "MATCH_ME";
+        Speciality speciality = new Speciality();
+        speciality.setDescription("Not a match");
+
+        Speciality savedSpecialty = new Speciality();
+        savedSpecialty.setId(1L);
+
+        given(specialityRepository.save(argThat(argument -> argument.getDescription().equals(MATCH_ME)))).willReturn(savedSpecialty);
+
+        //when
+        Speciality returnedSpecialty = service.save(speciality);
+
+        //then
+        assertThat(returnedSpecialty.getId()).isEqualTo(1L);
+    }
 }
